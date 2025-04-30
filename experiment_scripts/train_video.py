18c63f5d6b2258120582ad0e10f7933ad9b37e10
khoa <tuankhoanguyen94@gmail.com>
2025-05-01 03:24:52 +0900

    add note

--- experiment_scripts/train_video.py
+++ experiment_scripts/train_video.py
@@ -34,7 +34,7 @@ p.add_argument('--dataset', type=str, default='bikes',
 p.add_argument('--model_type', type=str, default='sine',
                help='Options currently are "sine" (all sine activations), "relu" (all relu activations,'
                     '"nerf" (relu activations and positional encoding as in NeRF), "rbf" (input rbf layer, rest relu)')
-p.add_argument('--sample_frac', type=float, default=38e-4,
+p.add_argument('--sample_frac', type=float, default=38e-4, ### 0.0038
                help='What fraction of video pixels to sample in each batch (default is all)')
 
 p.add_argument('--checkpoint_path', default=None, help='Checkpoint to trained model.')
